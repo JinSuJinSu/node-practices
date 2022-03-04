c9564ee5334901e0ebb980b27adb58356c15c2d8
JinsuJinsu <hjs928@naver.com>
2022-03-04 09:49:09 +0900

    route practice

--- helloweb-ex02/index.js
+++ helloweb-ex02/index.js
@@ -1,8 +1,11 @@
 const http = require("http");
 const path = require("path");
 const express = require("express");
+
+const mainRouter = require("./routes/main");
+const userRouter = require("./routes/user");
 const helloRouter = require("./routes/hello");
-const { nextTick } = require("process");
+
 const port = 9090;
 
 // Application Setup
@@ -13,12 +16,16 @@ const application = express()
     .use(express.urlencoded({extended:true})) // application/x-www-form-urlencoded
     .use(express.json()) // application/json
     // 3. view engine
+    .set("views",path.join(__dirname,'views'))
+    .set("view engine", "ejs")
     // 4. request router
     .all("*",function(req, res, next){
         res.locals.req = req;
         res.locals.res = res;
         next();
     })
+    .use("/",mainRouter)
+    .use("/user",userRouter)
     .use("/hello",helloRouter);
 
 // Server SetUp
